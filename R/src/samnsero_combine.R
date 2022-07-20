1b223b9edda665565e5057f607572c7ecdad6dd3
jimmyliu1326 <jimmyliu1326@yahoo.com>
2022-07-20 16:36:41 -0700

    ignore quast sample id modification

--- R/src/samnsero_combine.R
+++ R/src/samnsero_combine.R
@@ -11,14 +11,10 @@ args <- commandArgs(trailingOnly = T)
 
 # join all files by column modified id
 # IDs in quast results have _ instead of -
-combined_res <- map(rev(args), function(x) {
-	fread(x, header = T) %>% 
-		mutate(mod_id = str_replace_all(id, "-", "_"))
-	}) %>% 
-	reduce(left_join, by = "mod_id")
+combined_res <- map(rev(args), ~fread(., header = T)) %>% 
+	reduce(left_join, by = "id")
 
 # remove xtra columns
-id_cols <- c(colnames(combined_res)[which(str_detect(colnames(combined_res), "id."))], "mod_id")
 xtra_cols <- c("fasta_filepath")
 
 combined_res <- combined_res %>% 
