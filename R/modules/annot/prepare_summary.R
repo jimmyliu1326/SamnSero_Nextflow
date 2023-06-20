1f5d24dc4905f4cc1688b6320d1ce2e9007cb016
jimmyliu1326 <jimmyliu1326@yahoo.com>
2023-06-19 17:37:04 -0700

    exclude failed samples from annotation report

--- R/modules/annot/prepare_summary.R
+++ R/modules/annot/prepare_summary.R
@@ -2,15 +2,14 @@ source(file.path(src_dir, "src/samnsero_summary.R"))
 if (exists("amr_res_clean")) amr_drug_summary <- amr_class_sum(amr_res_clean)
 if (!is_empty(annot_summary)) {
 	# create presence/absence matrix of each annotation type
-	tidy_res <- annot_sum2mat(annot_summary, var_only = F)
-	plasmid_summary <- getSummaryType(tidy_res, "Plasmid")
-	amr_summary <- getSummaryType(tidy_res, "AMR")
-	vf_summary <- getSummaryType(tidy_res, "Virulence Factor")
+	tidy_res <- annot_sum2mat(annot_summary, var_only = F) %>% select(sistr_res_clean$id)
+	plasmid_summary <- getSummaryType(tidy_res, "Plasmid") %>% select(sistr_res_clean$id)
+	amr_summary <- getSummaryType(tidy_res, "AMR") %>% select(sistr_res_clean$id)
+	vf_summary <- getSummaryType(tidy_res, "Virulence Factor") %>% select(sistr_res_clean$id)
 	# create presence/absence matrix of each annotation type (variable elements only)
-	tidy_res_var <- annot_sum2mat(annot_summary, var_only = T)
-	plasmid_summary_var <- getSummaryType(tidy_res_var, "Plasmid")
-	amr_summary_var <- getSummaryType(tidy_res_var, "AMR")
-	vf_summary_var <- getSummaryType(tidy_res_var, "Virulence Factor")
-	
+	tidy_res_var <- annot_sum2mat(annot_summary, var_only = T) %>% select(sistr_res_clean$id)
+	plasmid_summary_var <- getSummaryType(tidy_res_var, "Plasmid") %>% select(sistr_res_clean$id)
+	amr_summary_var <- getSummaryType(tidy_res_var, "AMR") %>% select(sistr_res_clean$id)
+	vf_summary_var <- getSummaryType(tidy_res_var, "Virulence Factor") %>% select(sistr_res_clean$id)
 }
 
--- R/src/samnsero_summary.R
+++ R/src/samnsero_summary.R
@@ -52,6 +52,7 @@ amr_class_sum <- function(df) {
 		pivot_wider(names_from = "id",
 								values_from = "n",
 								values_fill = 0,
+								values_fn = ~return(.[1]),
 								names_expand = T) %>% 
 		column_to_rownames("Drug Class")
 }
